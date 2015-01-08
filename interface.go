a67ed9a48683e523b1d73b9957ac2021eb06d71a
Roger Peppe <rogpeppe@gmail.com>
2015-01-08 17:35:38 +0000

    initial commit

---
+++ interface.go
@@ -0,0 +1,28 @@
+package macarooncompat
+
+type Macaroon interface {
+	MarshalJSON() ([]byte, error)
+	MarshalBinary() ([]byte, error)
+	WithFirstPartyCaveat(caveatId string) (Macaroon, error)
+	WithThirdPartyCaveat(rootKey []byte, caveatId string, loc string) (Macaroon, error)
+	Bind(discharge Macaroon) (Macaroon, error)
+	Verify(rootKey []byte, check func(caveat string) error, discharges []Macaroon) error
+	Signature() []byte
+}
+
+type Package interface {
+	UnmarshalJSON(data []byte) (Macaroon, error)
+	UnmarshalBinary(data []byte) (Macaroon, error)
+	New(rootKey []byte, id, loc string) (Macaroon, error)
+}
+
+var Implementations =  []struct{
+	Name string
+	Pkg Package
+}{{
+	Name: "go",
+	Pkg: goMacaroonPackage{},
+}, {
+	Name: "libmacaroons",
+	Pkg: libMacaroonPkg{},
+}}
